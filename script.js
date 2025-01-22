f43a4c128ed0fe4ab02d7e0ab9090a1a531f55da
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-22 13:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+function createrandomPromise(names) {
+	const takentime = (Math.random() *2+1).toFixed(3);
+	return new Promise(resolve =>{
+		setTimeout(()=>{
+			resolve({names,takentime});
+		},takentime*1000);
+	});
+}
+
+const promise1 = createrandomPromise('Promise 1');
+const promise2 = createrandomPromise('Promise 2');
+const promise3 = createrandomPromise('Promise 3');
+
+Promise.all([promise1,promise2,promise3]).then((results)=>{
+	const totalTime = results.reduce((total,result) => total + parseFloat(result.takentime),0).toFixed(3);
+	const tableBody = document.getElementById('output');
+
+    results.forEach(result => {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${result.names}</td><td>${result.takentime}</td>`;
+        tableBody.appendChild(row);
+    });
+
+    const totalRow = document.createElement("tr");
+    totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
+    tableBody.appendChild(totalRow);
+
+});
